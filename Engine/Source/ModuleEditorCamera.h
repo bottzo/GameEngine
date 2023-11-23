ac1970aba2e1d7dec82fbfdeb64f84047e2d6273
bottzo <45202057+bottzo@users.noreply.github.com>
2023-11-23 19:02:32 +0100

    change computerç

--- Engine/Source/ModuleEditorCamera.h
+++ Engine/Source/ModuleEditorCamera.h
@@ -8,7 +8,7 @@
 class ModuleEditorCamera : public Module {
 	bool Init() override;
 
-	float4x4 LookAt(float3 eyePos, float3 targetPos, float3 upVector);
+	void LookAt(float3 eyePos, float3 targetPos, float3 upVector);
 private:
 	float4x4 cameraMatrix;
 	float4x4 projectionMatrixGL;
